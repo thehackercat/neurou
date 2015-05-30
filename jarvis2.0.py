6fd0bb8b187ff781ed67fb3f39b629c651be1f45
小虚大魔王 <lexuscyborg103@gmail.com>
2015-05-30 17:40:34 +0800

    update at 2015.05.30
    contributed to my roommate Nogizaka.

--- jarvis2.0.py
+++ jarvis2.0.py
@@ -1,32 +1,45 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*
+#然后我试试能不能扩张到100之内的奇偶数判定，如果可以，那就可以扩张到无限大的数的奇偶判定
 import random
-M=[0 for i in  range(10)]
-X=[1,2,3,4,5,6,7,8,9,10]
-Y=[0,1,0,1,0,1,0,1,0,1]
-st = 100
-o=[0 for i in  range(10)]
+M = [0 for i in range(100)]
+
+X = range(1,101)
+
+Y = []
+for x in range(100):
+	if x%2 == 0:
+		Y.append(0)
+	else:
+		Y.append(1)
+
+
+st = 1000
+
+o=[0 for i in range(100)]
+
 def init():
-	for n in range(10):
-		M[n]=random.randint(0,99)
-	
+	for n in range(100):
+		M[n] = random.randint(0,999)
+
 def active(m,n):
 	o=m*n
-	
+
 	if o>st:
 		return 1
 	else:
 		return 0
 
 def ca():
-	
-	for p in range(10):
-		o[p]=active(M[p], X[p])
-		
+
+	for p in range(100):
+		o[p] = active(M[p], X[p])
+
 def adjust():
 	err = 0
 	x = 0
-	for n in range(10):
-		if o[n]!=Y[n]:
+	for n in range(100):
+		if o[n]!= Y[n]:
 			err+=1
 			if 0==o[n]:
 				M[n]+=X[n]
@@ -34,13 +47,13 @@ def adjust():
 				M[n]-=X[n]
 		print M
 	return err
-	
+
 def test(a):
 	if(active(M[a-1], X[a-1])):
 		print "%d is an even number.Sir"%a
 	else:
 		print "%d is an odd number.Sir"%a
-	
+
 
 
 def main():
@@ -52,38 +65,27 @@ def main():
 	while(1):
 		n+=1
 		ca()
-		 
+
 	 	err = adjust()
 		if 0>=err:
 			break
-			
+
 	print "Training finished by %d times. Sir "%n
 	print 'Now you can test my neurou.Sir'
 	print '\n'
-	print 'Please input your number between 1-10'
+	print 'Please input your number between 1-100'
 	print '\n'
-	
+
 	while(1):
-		 
-		
+
+
 		a=raw_input()
 		a=int(a)
-		if a>10 or a<0:
+		if a>100 or a<0:
 			break
-		
+
 		test(a)
 		print '\n'
-	
+
 
 main()
-			
- 
-
- 
-
-
-
-		
-		 
-
-	 
